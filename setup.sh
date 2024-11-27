af6e88c67bb59dc615906900be2e1edeefbed1cf
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-11-27 19:50:30 +0200

    feat: Generate ssh key for GitHub

--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+echo "Setting dotfiles ..."
+
 dotfiles=$(find . -maxdepth 1 -type f -printf "%f\n" | grep "^\.")
 
 for dotfile in $dotfiles; do
    ln -vfs $PWD/$dotfile ~/$dotfile
 done
+
+echo "Create ssh key for GitHub ..."
+
+if [[ -f ~/.ssh/id_ed25519.pub ]]; then
+    echo "Key exists."
+else
+    echo "Creating ..."
+    ssh-keygen -t ed25519
+fi
+
+echo "Link the key with GitHub"
+echo "https://github.com/settings/keys"
