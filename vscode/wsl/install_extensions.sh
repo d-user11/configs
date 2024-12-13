d00f104d2df51cba92cbccb3da8580a62c05281a
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-12-13 15:53:11 +0200

    feat(vscode): Remove manually installed extensions

--- vscode/wsl/install_extensions.sh
+++ vscode/wsl/install_extensions.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-# TODO: Make use of code --list-extensions
+
 extensions_file="extensions.txt"
 
-if [[ -f $extensions_file ]]; then
+echo "[?] Checking for extensions to be removed ..."
+
+extensions_to_be_removed=$(diff <(code --list-extensions | sort) <(cat $extensions_file | sort) | grep "^< " | sed 's/^< //')
+
+if [[ ! -z "${extensions_to_be_removed}" ]]; then
+    printf "\n[!] Removing extensions ...\n\n"
+
+    for extension in $extensions_to_be_removed; do
+        code --uninstall-extension $extension
+        echo
+    done
+else
+    echo "[.] No extensions to be removed."
+fi
+
+printf "\n[+] Installing extensions ...\n\n"
+
+if [[ -s $extensions_file ]]; then
 
     while IFS= read -r extension; do
-        echo "Installing $extension ..."
         code --force --install-extension $extension
         echo
     done < "$extensions_file"
 
 else
-    echo "File $extensions_file does not exist."
-    exit 1
+    echo "[-] File $extensions_file might not exist or is empty."
 fi
