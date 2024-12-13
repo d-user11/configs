d38da4f39972fc5c9b4414a6614efc4a6e3615d9
d-user11 <60410862+d-user11@users.noreply.github.com>
2024-12-13 18:11:28 +0200

    feat: ask about setting github ssh key

--- setup.sh
+++ setup.sh
@@ -8,14 +8,25 @@ for dotfile in $dotfiles; do
    ln -vfs $PWD/$dotfile ~/$dotfile
 done
 
-echo "Create ssh key for GitHub ..."
+echo "Do you want to set up GitHub key?"
+select yn in "Yes" "No"; do
+    case $yn in
 
-if [[ -f ~/.ssh/id_ed25519.pub ]]; then
-    echo "Key exists."
-else
-    echo "Creating ..."
-    ssh-keygen -t ed25519
-fi
+        Yes )
+            echo "Create ssh key for GitHub ..."
 
-echo "Link the key with GitHub"
-echo "https://github.com/settings/keys"
+            if [[ -f ~/.ssh/id_ed25519.pub ]]; then
+                echo "Key exists."
+            else
+                echo "Creating ..."
+                ssh-keygen -t ed25519
+            fi
+
+            printf "Please follow the link and set the GitHub ssh key: "
+            echo "https://github.com/settings/keys"
+            break;;
+
+        No ) exit;;
+
+    esac
+done
